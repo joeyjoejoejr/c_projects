14ec33e9f788cd3e65740cff0bcb7e52f0bd249b
Joe Jackson <joe.jackson@scimedsolutions.com>
2014-01-28 20:08:30 -0500

    LCTHW Exercise 1

---
+++ lcthw/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  puts("Hello World.");
+  puts("Once upon a midnight dreary,");
+  puts("While I pondered weak and weary,");
+  puts("Over many ancient tomes of forgoten lore,");
+  puts("Suddenley there came a tapping,");
+  puts("As of someone gently rapping");
+
+  return 0;
+}
